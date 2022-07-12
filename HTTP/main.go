3303fc0a2c0126686acd625111a149f94daf1457
Chunt0 <chunt.mail@gmail.com>
2022-07-12 12:55:39 -0700

    Updated

--- HTTP/main.go
+++ HTTP/main.go
@@ -1,9 +1,19 @@
 package main
 
+// HTTP server basic jobs:
+// Process dynamic request -> incoming user requests, log in to accounts, post images
+// Serve statis assets -> serve JS, CSS, images to browsers to create a dynamic experience
+// Accept connections -> must listen on a specific port to accept connections
+
 import (
-	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
-	fmt.Println("hello")
+	r := mux.NewRouter()
+
+	r.HandleFunc()
+	http.ListenAndServe(":8080", nil)
 }
